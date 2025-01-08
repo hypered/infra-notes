283a4af11048425c308bae6c4ad7236c31bfd07c
Vo Minh Thu <thu@hypered.io>
2025-01-08 12:56:21 +0100

    XXX fixup.

--- tf/env/aaa/main.tf
+++ tf/env/aaa/main.tf
@@ -2,16 +2,6 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-#terraform {
-#  backend "s3" {
-#    region         = "eu-central-1"
-#    bucket         = "terraform.state.xyz"
-#    key            = "envs/aaa.tfstate"
-#    dynamodb_table = "terraform.lock"
-#    encrypt        = true
-#  }
-#}
-
 module "infra" {
   source = "../../desc"
   env_prefix = "aaa"
